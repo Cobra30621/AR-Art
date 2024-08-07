c842e72b692a4a8a26e9b2693ff7d80ed29ade70
Cobra <cobra20621@gmail.com>
2024-08-07 15:25:50 +0800

    feat: import project from old project

---
+++ Assets/Scripts/VideoData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "VideoData", menuName = "ScriptableObjects/VideoData", order = 1)]
+public class VideoData : ScriptableObject
+{
+    public VideoEntry[] videos;
+}
+
+[System.Serializable]
+public class VideoEntry
+{
+    public string videoId; // 使用字串來指定影片名稱
+    public string fileName; // 影片檔案名稱
+}
